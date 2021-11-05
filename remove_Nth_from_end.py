46c759c54ec52f38856ee5f9fc561fd2ae55b3e9
Aleksey Aleksandrov <55093100+aaaaaaaalesha@users.noreply.github.com>
2021-11-05 15:28:36 +0300

    Create remove_Nth_from_end.py
    Given the head of a linked list, remove the `n`th node from the end of the list and return its head.

---
+++ remove_Nth_from_end.py
@@ -0,0 +1,22 @@
+# Copyright 2021 aaaaaaaalesha
+
+from typing import Optional
+
+def remove_Nth_from_end(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    if head is None:
+        return head
+
+    prev_n = head
+    node = head
+
+    for _ in range(n):
+        node = node.next
+        if node is None:
+            head = head.next
+            return head
+
+    while node.next is not None:
+        node, prev_n = node.next, prev_n.next
+
+    prev_n.next = prev_n.next.next
+    return head
